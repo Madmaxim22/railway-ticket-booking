37a813a6225a34c8a2c6884f2f5978233b33957e
Максим Мухомедьяров <madmaxim22@yandex.ru>
2026-04-18 17:03:33 +0300

    feat: add header component with styling and layout, include app styling and assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import Header from './pages/Header'
+import HomePage from './pages/HomePage'
+import Footer from './pages/Footer'
+import './App.css'
 
 function App() {
   return (
     <>
+    <div className="app">
+      <Header />
+      <HomePage />
+      <Footer />
+    </div>
     </>
   )
 }
